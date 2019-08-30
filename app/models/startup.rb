dfcae6479f648af9263b9cf05eee3980942cd96e
John Karrell Speck <speckworks@gmail.com>
2019-08-30 19:32:57 +0000

    Automatically backed up by Learn

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,27 @@
 class Startup
-
+    attr_reader :name, :founder, :domain
+      @@all = []
+      
+    def initialize(name, founder, domain)
+      @name = name
+      @founder = founder
+      @domain = domain
+      @all << self
+    end
+    
+    def pivot(domain, name)
+      @name = name
+      @domain = domain
+    end
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,5 +7,15 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+Startup.all
+uber = Startup.new("Uber", "Dude McDude", "Bay Area")
+Startup.new("Yelp", "Sally Mae", "Las Vegas")
+Startup.new("Seamless", "John Smith", "NYC")
+
+Startup.pivot("LA","Uber")
+
+
+
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
